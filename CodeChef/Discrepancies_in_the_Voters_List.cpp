c180e03e18ebe844e2c4f06230045a3547efb49d
nicklauszhanghoule <58124714+nicklauszhanghoule@users.noreply.github.com>
2020-05-10 16:26:23 -0400

    Update Discrepancies_in_the_Voters_List.cpp

--- CodeChef/Discrepancies_in_the_Voters_List.cpp
+++ CodeChef/Discrepancies_in_the_Voters_List.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
-int arr[100000000];
 int main() {
-    int a, b, c;
-    cin >> a >> b >> c;
-    for(int i = 0; i < a+b+c; ++i ) {
-       int t;
-       cin >> t;
-       ++arr[t];
+    int n, x , y , z;
+    cin >> x >> y >> z;
+    n = x + y + z;
+    int a[n];
+    for(int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    vector<int> v;
+    for(int i = 1; i <  n-1; ++i) {
+        if(a[i-1] == a[i] && a[i+1] != a[i]) {
+            v.push_back(a[i]);
+        }
+    }
+    cout << v.size() << "\n";
+    for(int i = 0; i < v.size(); ++i) {
+        cout << v[i] << "\n";
     }
     return 0;
 }
