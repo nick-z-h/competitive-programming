345ab7f54e3cb521fbdd86364e83260a46cae578
nicklauszhanghoule <58124714+nicklauszhanghoule@users.noreply.github.com>
2020-07-24 17:24:32 -0400

    Add files via upload

---
+++ Kattis/carrot.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        int n, p;
+        cin >> n >> p;
+        for(int i = 0; i < n; ++i) {
+            string t;
+            cin >> t;
+        }
+        cout << p << "\n";
+        return 0;
+}
---
+++ Kattis/thelastproblem.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        string s;
+        getline(cin, s);
+        cout << "Thank you, " << s <<  ", and farewell!" << "\n";
+        return 0;
+}
