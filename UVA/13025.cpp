03279ce6e28dfa003045eec18925893eb27a3a39
nicklauszhanghoule <58124714+nicklauszhanghoule@users.noreply.github.com>
2020-07-24 17:25:55 -0400

    Add files via upload

---
+++ UVA/13025.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout << "May 29, 2013 Wednesday\n";
+        return 0;
+}
